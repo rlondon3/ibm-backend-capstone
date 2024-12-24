695f31c4d6ba4041678785b0a0e97eed19d62f1d
rlondon3 <codesmithr@outlook.com>
2024-12-23 19:31:31 -0500

    add user auth with jwt

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -22,15 +22,8 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-
-// authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
-//{{insert code here}}
-
-// Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
+const authRoutes = require('./routes/authRoutes');
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
 const searchRoutes = require('./routes/searchRoutes');
 
 
@@ -39,15 +32,9 @@ const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
-// Use Routes
-// authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
-//{{insert code here}}
-
-// Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
+//Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
 app.use('/api/secondchance/search', searchRoutes);
 
 // Global Error Handler
